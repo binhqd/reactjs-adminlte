6fdf62e0c27ee16066864e0ef86ee6b2219a6d9c
Binh Quan <binhqd@gmail.com>
2018-03-13 16:00:58 +0700

    Use createBrowserHistory from history instead of using existing browserHistory from react-router

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import { Router, browserHistory } from 'react-router';
+import { createBrowserHistory } from 'history';
+import { Router } from 'react-router-dom';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { Provider } from 'react-redux';
 import routes, {store} from 'base/routes/index.jsx';
 
 // Create an enhanced history that syncs navigation events with the store
-const history = syncHistoryWithStore(browserHistory, store);
+const history = syncHistoryWithStore(createBrowserHistory(), store);
 
 ReactDOM.render((
   <Provider store={store}>
